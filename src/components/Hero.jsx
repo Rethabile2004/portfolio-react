2608e8080efe6c8d0a520067fb598f679a0a68c8
Rethabile Siase <ericksiase375@gmail.com>
2025-11-13 16:42:24 +0200

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import heroImg from '../assets/hero.svg';
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
+
+const Hero = () => {
+  return (
+    <div className='bg-emerald-100 py-24'>
+        <div className='mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8'></div>
+    </div>
+  )
+}
+
+export default Hero
